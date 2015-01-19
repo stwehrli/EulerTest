c249f5f572c4e66dce3a57a9897091a61b48347d
stwehrli <stwehrli@gmail.com>
2015-01-19 23:32:59 +0100

    Update Test.R

--- Test.R
+++ Test.R
@@ -1,5 +1,7 @@
 # This file is now generated in RStudio. 
 # We will see if we can commit it back to GitHub
 
-# Some random variates.
-rnorm(100)
+# A simple function that returns random variates
+a <- function() {
+ rnorm(100)
+}
